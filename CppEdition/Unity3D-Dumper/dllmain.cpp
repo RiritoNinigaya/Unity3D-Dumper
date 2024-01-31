a38f7b3766f88dcfeb41ab6fd4355881a094fc3f
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-01-31 11:27:17 +0200

    Add files via upload

---
+++ CppEdition/Unity3D-Dumper/dllmain.cpp
@@ -0,0 +1,35 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include <Windows.h>
+#include <iostream>
+#include <filesystem>
+#include <thread>
+#include "Functions.h"
+using namespace std;
+namespace fs = std::filesystem;
+
+void Main() {
+    fs::create_directory("C:\\Unity3DDumper");
+    I::DumpToFile("C:\\Unity3DDumper\\DumpHoloEarth.cs"); //You Can Also Rename You're Dumped File!!! :D
+    MessageBoxA(0, "Dumped!!!", "Unity3D-Dumper", MB_OK | MB_ICONINFORMATION);
+}
+void GetThread() {
+    std::thread main_func(&Main);
+    main_func.detach();
+}
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(GetThread), 0, 0, 0);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
